6f2f42e5d8520ee749f6cd0be92a1d48868f3982
“Sadia <Sadia.islam.badhon@gmail.com>
2019-11-05 14:49:38 -0500

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,31 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery 
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  def self.total_price
+    price_array = all.map { |painting|
+    painting.price 
+    }
+    # sum = 0
+    # price_array.each do |price|
+    #   sum += price
+    # end
+    # sum
+    # price_array.reduce(:+)
+    price_array.sum
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,16 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Bob", 2)
+a2 = Artist.new("Tim", 3)
+a3 = Artist.new("Rob", 4)
 
+g1 = Gallery.new("Cl gallery","soho")
+g2 = Gallery.new("flatiron","wallstreet")
+g3 = Gallery.new("sony","uptown")
 
+p1 = Painting.new("Mona", 4, a1, g1)
+p2 = Painting.new("Lisa", 5, a2, g2)
+p3 = Painting.new("Eric", 6, a3, g3)
 
 binding.pry
 
