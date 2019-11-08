3aacc736f352f1b3f084c0b79065aa11229ab6c5
Dominic Chu <dominic.m.chu@gmail.com>
2019-11-08 17:15:34 -0500

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,36 @@
 class Gallery
-
+  @@all = []
   attr_reader :name, :city
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
+  def self.all
+    @@all
+  end 
+  def paintings
+    Painting.all.select do |instance|
+      instance.gallery == self
+    end 
+  end 
+  def artists
+    paintings.map do |instance|
+      instance.artist
+    end 
+  end 
+  def artist_names
+    artists.map do |instance|
+      instance.name
+    end 
+  end 
+
+  def most_expensive_painting
+    paintings.max_by do |instance|
+      instance.price
+    end 
+  end 
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,23 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("A", 1)
+a2 = Artist.new("B", 2)
+a3 = Artist.new("C", 3)
+a4 = Artist.new("D", 4)
+
+g1 = Gallery.new("G", "Gettysburg")
+g2 = Gallery.new("H", "Hopetown")
+g3 = Gallery.new("I", "Incheon")
+g4 = Gallery.new("J", "Jarlsburg")
+
+p1 = Painting.new("P", 4000, a1, g1)
+p2 = Painting.new("Q", 1000, a2, g2)
+p3 = Painting.new("R", 2000, a3, g3)
+p4 = Painting.new("S", 3000, a4, g4)
+p5 = Painting.new("T", 5000, a1, g2)
+
+
+
 
 
 
